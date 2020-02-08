5504379782aaba8f1900ac3799799e9adf277ab1
loeken <loeken@internetz.me>
2020-02-08 21:49:27 +0100

    passing on environment via NODE_ENV

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
+const environment = process.env.NODE_ENV || "development";
 
 const requestHandler = (request, response) => {
   console.log(request.url);
-  response.end('Hello Node.js Server!')
+  response.end('Hello Node.js Server! env: ' + environment)
 };
 
 const server = http.createServer(requestHandler);
